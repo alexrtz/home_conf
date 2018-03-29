13b8aa64bafc981bbb86e230a431f0b830d58df4
Alexandre Ortiz <alexandre.ortiz@alexrtz.net>
2018-03-28 22:37:03 -0400

    Paths update + emacs cpp

--- shell_common/variables.sh
+++ shell_common/variables.sh
@@ -1,7 +1,19 @@
 EDITOR="emacs -nw -q"
 SHELL_USERNAME=alex
 
-PATH=$MYPATH:~/.config/mine/git/git.d/git-number:$PATH:/usr/local/bin
+PATH=~/.config/mine/git/git.d/git-number:$PATH:/usr/local/bin
+
+if [ -n "$MY_PATH" ];
+then
+    PATH=$MY_PATH:$PATH
+fi
+
+LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:$LD_LIBRARY_PATH
+
+if [ -n "$LD_LIBRARY_PATH" ];
+then
+    LD_LIBRARY_PATH=$MY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+fi
 
 LD_LIBRARY_PATH=$MY_LD_LIBRARY_PATH:/usr/lib:/usr/local/lib:$LD_LIBRARY_PATH
 LIBRARY_PATH=$MY_LIBRARY_PATH:/usr/lib:/usr/local/lib:$LIBRARY_PATH
