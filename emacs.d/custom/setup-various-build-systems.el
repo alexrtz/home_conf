ca5985e54fcdfdf20835b8fdf9c7f2f56071fc8e
Alexandre Ortiz <alexandre.ortiz@alexrtz.net>
2019-10-14 21:48:43 -0400

    Bring back the CMake hook

--- emacs.d/custom/setup-various-build-systems.el
+++ emacs.d/custom/setup-various-build-systems.el
@@ -1,22 +1,20 @@
 ;; CMake
 
-;; (use-package cmake-mode)
-;; (setq auto-mode-alist (cons '("\\CMakeLists.txt\\w?" . cmake-mode) auto-mode-alist))
-;; (setq auto-mode-alist (cons '("\\.cmake\\w?" . cmake-mode) auto-mode-alist))
+(defun my-cmake-mode-hook()
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq show-trailing-whitespace t)
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
+)
 
-;; (defun my-cmake-mode-hook()
-;;   (setq tab-width 2)
-;;   (setq c-basic-offset 2)
-;;   (setq indent-tabs-mode nil)
-;;   (setq show-trailing-whitespace t)
-;; ;  (add-hook 'before-save-hook 'delete-trailing-whitespace)
-;; )
+(add-hook 'cmake-mode-hook 'my-cmake-mode-hook)
+
 
 ;; Docker
 (use-package dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 
-(add-hook 'cmake-mode-hook 'my-cmake-mode-hook)
 
 ;; QMake
 
