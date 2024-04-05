da139005add7216a307e1b4f88720f6fd289515f
Alexandre Ortiz <alexandre.ortiz@alexrtz.net>
2024-04-04 22:33:51 -0400

    Add a missing config file

---
+++ emacs.d/custom/container-setup.el
@@ -0,0 +1,11 @@
+;;; container-setup --- Config for the formats used for containers
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package dockerfile-mode)
+(use-package docker-compose-mode)
+
+(provide 'container-setup)
+;;; container-setup.el ends here
